c4fb773b29ed8a859dc58ce2f21593633e5fe81f
Alexander Babai <sustem34@gmail.com>
2015-10-06 16:46:33 +0300

    Added Time and Velocity stuff

--- physics/library/core/quantities/impl/Time.java
+++ physics/library/core/quantities/impl/Time.java
@@ -22,5 +22,13 @@ public class Time extends Quantity {
 	public void compute(Velocity v, Velocity v0, Acceleration a) {
 		value = (v.getValue() - v0.getValue()) / a.getValue();
 	}
+	
+	public void compute(Velocity v, Velocity v0, Distance d) {
+		value = 2 * d.getValue() / (v.getValue() + v0.getValue());
+	}
+	
+	public void compute(Work a, Power p) {
+		value = a.getValue() / p.getValue();
+	}
 
 }
--- physics/library/core/quantities/impl/Velocity.java
+++ physics/library/core/quantities/impl/Velocity.java
@@ -18,9 +18,25 @@ public class Velocity extends Quantity {
 		super(value, unit);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public void compute(Velocity v0, Acceleration a, Time t) {
 		value = v0.getValue() + a.getValue() * t.getValue();
 	}
 
+	public void compute(Velocity v0, Acceleration a, Distance d) {
+		value = Math.sqrt(Math.pow(v0.getValue(), 2) + 2 * a.getValue()
+				* d.getValue());
+	}
+
+	public void compute(Velocity v0, Distance d, Time t) {
+		value = 2 * d.getValue() / t.getValue() - v0.getValue();
+	}
+	
+	public void compute(Acceleration a, Radius r) {
+		value = Math.sqrt(a.getValue() * r.getValue());
+	}
+	
+	public void compute(Pulse p, Mass m) {
+		value = p.getValue() / m.getValue();
+	}
 }
