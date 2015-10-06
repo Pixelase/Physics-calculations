e7ffb1767af0dfbd52aac748a1fa1a0af952b48c
Alexander Babai <sustem34@gmail.com>
2015-10-06 21:11:46 +0300

    Added an example of using this library
    На примере консольного приложения

--- physics/library/core/quantities/TimeModifiers.java
+++ physics/library/core/quantities/TimeModifiers.java
@@ -2,6 +2,6 @@ package physics.library.core.quantities;
 
 public abstract class TimeModifiers {
 	public static final double SECONDS = 1;
-    public static final double MINUTES = 0.016666667;
-    public static final double HOURS = 0.000277778;
+    public static final double MINUTES = 60;
+    public static final double HOURS = 3600;
 }
